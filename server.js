022cf8d26a49e1577d8ff0767131e5276341af71
sowunmi mayowa <smayowa176@gmail.com>
2024-09-12 04:05:29 +0100

    update

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json())
 
 //static for web view
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/dist/index.html')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/dist/index.html')));
+}
 
 
 console.log(__dirname)
